04e8e0b451870d6fc6425271fd17ce890b89ae31
Vladislav Kramorenko <85196001+Xewus@users.noreply.github.com>
2023-01-19 09:57:04 +0300

    Create E.py

---
+++ E.py
@@ -0,0 +1,22 @@
+# (E) Отчет (15 баллов), Полное решение: 15 баллов,
+
+def main():
+    for _ in range(int(input())):
+        tasks = set()
+        amount = int(input())
+        data = input().split()
+        tasks.add(data[0])
+
+        for day in range(1, amount):
+            if data[day] == data[day - 1]:
+                continue
+            if data[day] in tasks:
+                print('NO')
+                break
+            tasks.add(data[day])
+        else:
+            print('YES')
+
+
+if __name__ == '__main__':
+    main()
