b0a17bcd111f805c9978882b7b548d692e5d3de3
Vladislav Kramorenko <85196001+Xewus@users.noreply.github.com>
2023-01-23 18:40:13 +0300

    Create solution.py

---
+++ Ozone_Route256_2023/Task_B/solution.py
@@ -0,0 +1,22 @@
+# Полное решение: 10 баллов.
+
+def solution() -> int:
+    basket = {}
+    for product in map(int, input().split()):
+        if product in basket:
+            basket[product] += 1
+            continue
+        basket[product] = 1
+    return sum(
+        (amount - amount // 3) * prod for prod, amount in basket.items()
+    )
+
+
+def main():
+    for _ in range(int(input())):
+        input()
+        print(solution())
+
+
+if __name__ == '__main__':
+    main()
