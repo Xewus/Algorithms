907e6555c375591fe51cde6fec35534924eafbed
Vladislav Kramorenko <85196001+Xewus@users.noreply.github.com>
2023-02-19 00:37:10 +0300

    Update solution.go

--- Ozone_Route256_2023_Middle/Task_G/solution.go
+++ Ozone_Route256_2023_Middle/Task_G/solution.go
@@ -2,7 +2,7 @@
 // Полное решение: 25 баллов
 
 package main
- 
+
 import (
 	"bufio"
 	"errors"
@@ -11,31 +11,36 @@ import (
 	"sort"
 	"strings"
 )
- 
+
 type Record struct {
 	index  int
 	window int
 	moved  rune
 }
- 
+
 func main() {
 	var amountTests uint16
 	var amountWindows, amountRecords int
-	input := bufio.NewReader(os.Stdin)
- 
+	f := os.Stdin
+	// f, err := os.Open("19")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	input := bufio.NewReader(f)
+
 	fmt.Fscanln(input, &amountTests)
- 
+
 	for ; amountTests > 0; amountTests-- {
 		fmt.Fscan(input, &amountWindows, &amountRecords)
 		windows := make([]bool, amountWindows, amountWindows)
 		records := make([]Record, amountRecords, amountRecords)
- 
+
 		for i := range records {
 			records[i] = Record{index: i, moved: '0'}
 			fmt.Fscan(input, &records[i].window)
 			records[i].window--
 		}
- 
+
 		if betterAssist(&records, &windows) != nil {
 			fmt.Println("x")
 		} else {
@@ -43,13 +48,13 @@ func main() {
 		}
 	}
 }
- 
+
 func betterAssist(pRecords *[]Record, pWindows *[]bool) error {
 	records := *pRecords
 	windows := *pWindows
- 
+
 	sort.Slice(records, func(i, j int) bool { return records[i].window < records[j].window })
- 
+
 	for i := 0; i < len(records); i++ {
 		index := records[i].window
 		switch {
@@ -68,10 +73,10 @@ func betterAssist(pRecords *[]Record, pWindows *[]bool) error {
 	sort.Slice(records, func(i, j int) bool { return records[i].index < records[j].index })
 	return nil
 }
- 
+
 func outRecords(pRecords *[]Record) {
 	builder := strings.Builder{}
- 
+
 	for _, record := range *pRecords {
 		builder.WriteByte(byte(record.moved))
 	}
